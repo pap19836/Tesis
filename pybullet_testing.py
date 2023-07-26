6788897f67d60a05687298ee6d0d70dc9a274656
pap19836 <pap19836@uvg.edu.gt>
2023-07-26 13:47:12 -0600

    Finished the sliders in pybullet. Ahora toca hacer el URDF del robonova

--- pybullet_testing.py
+++ pybullet_testing.py
@@ -1,5 +1,6 @@
 import pybullet
 import pybullet_data
+import math
 
 pybullet.connect(pybullet.GUI)
 pybullet.resetSimulation()
@@ -12,16 +13,32 @@ def add_robot(self):
 pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
 plane = pybullet.loadURDF("plane.urdf")
 
-pybullet.setAdditionalSearchPath("C:\\Users\\stefa\\OneDrive - Universidad del Valle de Guatemala\\UVG\\Tesis\\kuka_experimental")
-robot = pybullet.loadURDF("\\kuka_kr6_support\\urdf\\kr6r700sixx.xacro")
+#robot = pybullet.loadURDF("xpp/robots/xpp_hyq/urdf/biped.urdf")
+robot = pybullet.loadURDF("kuka_experimental/kuka_kr120_support/urdf/kr120r2500pro.urdf",[0,0,3],useFixedBase=1)
+position, orientation = pybullet.getBasePositionAndOrientation(robot)
+numJoints = pybullet.getNumJoints(robot)
 
+joints_info = []
+for i in range(numJoints):
+    x = pybullet.getJointInfo(robot,i)
+    joints_info.append(x)
+joint_number = list(range(numJoints))
 pybullet.setGravity(0,0,-9.81)
 pybullet.setTimeStep(0.0001)
 pybullet.setRealTimeSimulation(1)
-# pybullet.setJointMotorControlArray(
-#     robot, range(6), pybullet.POSITION_CONTROL,
-#     targetPositions=[0.1] * 6)
-# 
-# for _ in range(10000):
-#     pybullet.stepSimulation()
-#
+slider_parameters = []
+for i in range(numJoints):
+    x = pybullet.addUserDebugParameter(' '+str(joints_info[i][1], 'utf-8'),joints_info[i][8],joints_info[i][9], 0)
+    slider_parameters.append(x)
+
+
+
+#    robot, range(6), pybullet.POSITION_CONTROL,
+#    targetPositions=[0.1] * 6)
+while True:
+    pybullet.stepSimulation()
+    slider_values = []
+    for i in range(numJoints):
+        x = pybullet.readUserDebugParameter(i)
+        slider_values.append(x)
+    pybullet.setJointMotorControlArray(robot,joint_number,pybullet.POSITION_CONTROL, slider_values)
